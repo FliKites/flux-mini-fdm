a8f535e7b07e246dc25a7343223e08647ed957db
FliKites <72367027+FliKites@users.noreply.github.com>
2023-01-31 04:57:10 -0700

    Added self signed certs with TLSA Record
    This allows users more control over how they handle certificates for their deployment. 
    
    The `CERT=none` option allows users to upload their own certificates into the container.
    
    `CERT=self` generates a self signed certificate and generates a corresponding TLSA record with the 3 0 1 prefix. 
    
    This adds Handshake Domain support with SSL compatibility out of the box, with no need for an ACME server.

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -1,36 +1,69 @@
 #!/usr/bin/env bash
 
 if [ -n "$DOMAIN" ]; then
-    if [ "$STAGING" = true ]; then
+    if [ "$CERT" = "none" ]; then
+        mkdir -p /etc/haproxy/certs
+    elif [ "$CERT" = "self" ]; then
+     mkdir -p /etc/letsencrypt/live  && mkdir -p /etc/haproxy/certs && 
+openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
+  -keyout /etc/letsencrypt/live/cert.key -out /etc/letsencrypt/live/cert.crt -extensions ext  -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[ext]";
+    echo "keyUsage=critical,digitalSignature,keyEncipherment";
+    echo "extendedKeyUsage=serverAuth";
+    echo "basicConstraints=critical,CA:FALSE";
+    echo "subjectAltName=DNS:$DOMAIN";
+    ) -subj "/CN=$DOMAIN" && cat /etc/letsencrypt/live/cert.key \
+              /etc/letsencrypt/live/cert.crt \
+              | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
+
+        echo "Self-signed certificate generated and stored in /etc/haproxy/certs/"
+        echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
+openssl x509 -in /etc/letsencrypt/live/cert.crt -outform DER | openssl sha256 | sed 's/(stdin)=//' | awk '{print "3 0 1",$1}'
+
+    elif [ "$STAGING" = true ]; then
         if [ -n "$ACME" ]; then
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
             --staging \
-            -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL"
+            -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/haproxy/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
+              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
+              | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
         else
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
             --staging \
-            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL"
+            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/haproxy/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
+              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
+              | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
         fi
     else
         if [ -n "$ACME" ]; then
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
-            -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL"
+            -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/haproxy/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
+              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
+              | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null && echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
+openssl x509 -in /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem -outform DER | openssl sha256 | sed 's/(stdin)=//' | awk '{print "3 0 1",$1}'
         else
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
-            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL"
+            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/haproxy/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
+              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
+              | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
         fi
     fi
 
-    mkdir -p /etc/haproxy/certs
-    for site in `ls -1 /etc/letsencrypt/live | grep -v ^README$`; do
-        cat /etc/letsencrypt/live/$site/privkey.pem \
-          /etc/letsencrypt/live/$site/fullchain.pem \
-          | tee /etc/haproxy/certs/haproxy-"$site".pem >/dev/null
-    done
+    if [ "$CERT" != "none" ]; then
+        mkdir -p /etc/haproxy/certs
+    else
+        for site in `ls -1 /etc/letsencrypt/live | grep -v ^README$`; do
+            cat /etc/letsencrypt/live/$site/privkey.pem \
+              /etc/letsencrypt/live/$site/fullchain.pem \
+              | tee /etc/haproxy/certs/haproxy-"$site".pem >/dev/null
+        done
+    fi
 fi
 
 exit 0
