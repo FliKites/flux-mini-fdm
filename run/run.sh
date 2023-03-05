a57593c43fa48c4d0bcda4f07d847c50504625af
FliKites <72367027+FliKites@users.noreply.github.com>
2023-03-05 00:24:01 -0700

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -1,1 +1,1 @@
-docker run --name lb -d -e APP_NAME=nostr -e APP_PORT=35860 -e DOMAIN=nostr.hssl -e STAGING=false -e STICKY=false -e CERT=self -p 80:80 -p 443:443 wirewrex/flux-mini-fdm:handshake
+docker run --name lb -d -e APP_NAME=nostr -e APP_PORT=35860 -e DOMAIN=nostr.hssl -e STAGING=false -e STICKY=false -e CERT=self -p 80:80 -p 443:443 <your-image>
