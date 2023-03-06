ce0ef9eb29fc1319c84b4761e6a73e66ac83849a
FliKites <72367027+FliKites@users.noreply.github.com>
2023-03-05 21:27:42 -0700

    Stop self signed certs from being deleted
    This update stops self signed certs from being regenerated every time the container restarts. It also generates the output of the TLSA in the logs for all types of certificates, not just self signed ones.

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -6,11 +6,8 @@ if [ -n "$DOMAIN" ]; then
               /etc/letsencrypt/live/"$DOMAIN".crt \
               | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
               echo "Self-signed certificate manually loaded and stored in /etc/haproxy/certs/"
-             TLSA=$(openssl x509 -noout -pubkey -in /etc/haproxy/certs/haproxy-"$DOMAIN".pem | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | sed '' | awk '{print "3 1 1",$1}')
-             echo $TLSA > /etc/letsencrypt/live/"$DOMAIN"_TLSA.txt
-             echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
-             echo $TLSA
-    elif [ "$CERT" = "self" ]; then
+
+    elif [ "$CERT" = "self" ] && [ -z "$(ls -A /etc/haproxy/certs)" ]; then
      mkdir -p /etc/letsencrypt/live && mkdir -p /etc/haproxy/certs &&
 openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
   -keyout /etc/letsencrypt/live/"$DOMAIN".key -out /etc/letsencrypt/live/"$DOMAIN".crt -extensions ext  -config \
@@ -24,12 +21,8 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
     ) -subj "/CN=$DOMAIN" && cat /etc/letsencrypt/live/"$DOMAIN".key \
               /etc/letsencrypt/live/"$DOMAIN".crt \
               | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
-
-        echo "Self-signed certificate generated and stored in /etc/haproxy/certs/"
-       TLSA=$(openssl x509 -noout -pubkey -in /etc/haproxy/certs/haproxy-"$DOMAIN".pem | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | sed '' | awk '{print "3 1 1",$1}')
-             echo $TLSA > /etc/letsencrypt/live/"$DOMAIN"_TLSA.txt
-             echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
-             echo $TLSA
+    echo "Self-signed certificate generated and stored in /etc/haproxy/certs/"
+    
     elif [ "$STAGING" = true ]; then
         if [ -n "$ACME" ]; then
             certbot certonly --no-self-upgrade -n --text --standalone \
@@ -53,10 +46,7 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
             -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/haproxy/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
               /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
               | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
-            TLSA=$(openssl x509 -noout -pubkey -in /etc/haproxy/certs/haproxy-"$DOMAIN".pem | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | sed '' | awk '{print "3 1 1",$1}')
-             echo $TLSA > /etc/letsencrypt/live/"$DOMAIN"_TLSA.txt
-             echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
-             echo $TLSA
+
         else
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
@@ -65,6 +55,10 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
               | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null && echo "Letsencrypt certificate generated and stored in /etc/haproxy/certs/"
         fi
     fi
+             TLSA=$(openssl x509 -noout -pubkey -in /etc/haproxy/certs/haproxy-"$DOMAIN".pem | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | sed '' | awk '{print "3 1 1",$1}')
+             echo $TLSA > /etc/letsencrypt/live/"$DOMAIN"_TLSA.txt
+             echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
+             echo $TLSA
 fi
 
 exit 0
