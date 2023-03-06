e2f5f69a0d84dc0ddbb3a0b16be689d7b6fa4add
FliKites <72367027+FliKites@users.noreply.github.com>
2023-03-05 21:47:24 -0700

    Removed redundant code + changed TLSA type
    TLSA records now use 3 1 1 prefix instead of 3 0 1

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -3,10 +3,10 @@
 if [ -n "$DOMAIN" ]; then
     if [ "$CERT" = "none" ]; then
         mkdir -p /etc/nginx/certs
-    elif [ "$CERT" = "self" ]; then
-     mkdir -p /etc/letsencrypt/live  && mkdir -p /etc/nginx/certs && 
+    elif [ "$CERT" = "self" ] && [ -z "$(ls -A /etc/nginx/certs)" ]; then
+     mkdir -p /etc/letsencrypt/live/"$DOMAIN"  && mkdir -p /etc/nginx/certs &&
     openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
-  -keyout /etc/letsencrypt/live/nginx-"$DOMAIN".key -out /etc/letsencrypt/live/nginx-"$DOMAIN".crt -extensions ext  -config \
+  -keyout /etc/letsencrypt/live/"$DOMAIN"/nginx-"$DOMAIN".key -out /etc/letsencrypt/live/"$DOMAIN"/nginx-"$DOMAIN".crt -extensions ext  -config \
   <(echo "[req]";
     echo distinguished_name=req;
     echo "[ext]";
@@ -14,61 +14,42 @@ if [ -n "$DOMAIN" ]; then
     echo "extendedKeyUsage=serverAuth";
     echo "basicConstraints=critical,CA:FALSE";
     echo "subjectAltName=DNS:$DOMAIN";
-    ) -subj "/CN=$DOMAIN" && cat /etc/letsencrypt/live/nginx-"$DOMAIN".key \
-              /etc/letsencrypt/live/nginx-"$DOMAIN".crt \
-              | tee /etc/nginx/certs/nginx-"$DOMAIN".pem >/dev/null
+    ) -subj "/CN=$DOMAIN" && cp /etc/letsencrypt/live/"$DOMAIN"/* /etc/nginx/certs/ 
+    echo "Self-signed certificate generated and stored in /etc/nginx/certs/"
+    echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
+    TLSA=$(openssl x509 -noout -pubkey -in /etc/letsencrypt/live/"$DOMAIN"/nginx-"$DOMAIN".crt | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | sed '' | awk '{print "3 1 1",$1}')
+    echo $TLSA > /etc/letsencrypt/"$DOMAIN"_TLSA.txt
+    echo $TLSA
 
-        echo "Self-signed certificate generated and stored in /etc/nginx/certs/"
-        echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
-        TLSA=$(openssl x509 -in /etc/letsencrypt/live/nginx-"$DOMAIN".crt -outform DER | openssl sha256 | sed 's/(stdin)=//' | awk '{print "3 0 1",$1}')
-        echo $TLSA > /etc/letsencrypt/"$DOMAIN"_TLSA.txt
-        echo $TLSA
     elif [ "$STAGING" = true ]; then
         if [ -n "$ACME" ]; then
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
             --staging \
-            -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/nginx/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
-              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
-              | tee /etc/nginx/certs/nginx-"$DOMAIN".pem >/dev/null
+            -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/nginx/certs && cp /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/nginx/certs/nginx-"$DOMAIN".crt
+             cp /etc/letsencrypt/live/"$DOMAIN"/privkey.pem /etc/nginx/certs/nginx-"$DOMAIN".key
         else
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
             --staging \
-            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/nginx/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
-              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
-              | tee /etc/nginx/certs/nginx-"$DOMAIN".pem >/dev/null
+            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/nginx/certs && cp /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/nginx/certs/nginx-"$DOMAIN".crt
+             cp /etc/letsencrypt/live/"$DOMAIN"/privkey.pem /etc/nginx/certs/nginx-"$DOMAIN".key
         fi
     else
         if [ -n "$ACME" ]; then
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
-            -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/nginx/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
-              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
-              | tee /etc/nginx/certs/nginx-"$DOMAIN".pem >/dev/null && echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
-            
-            TLSA=$(openssl x509 -in /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem -outform DER | openssl sha256 | sed 's/(stdin)=//' | awk '{print "3 0 1",$1}')
+            -d "$DOMAIN" --server "$ACME" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/nginx/certs && cp /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/nginx/certs/nginx-"$DOMAIN".crt
+             cp /etc/letsencrypt/live/"$DOMAIN"/privkey.pem /etc/nginx/certs/nginx-"$DOMAIN".key && echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
+           TLSA=$(openssl x509 -noout -pubkey -in /etc/letsencrypt/live/"$DOMAIN"/nginx-"$DOMAIN".crt | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | sed '' | awk '{print "3 1 1",$1}')
            echo $TLSA > /etc/letsencrypt/"$DOMAIN"_TLSA.txt
-           echo $TLSA 
+           echo $TLSA
         else
             certbot certonly --no-self-upgrade -n --text --standalone \
             --preferred-challenges http-01 \
-            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/nginx/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
-              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
-              | tee /etc/nginx/certs/nginx-"$DOMAIN".pem >/dev/null
+            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/nginx/certs && cp /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/nginx/certs/nginx-"$DOMAIN".crt
+             cp /etc/letsencrypt/live/"$DOMAIN"/privkey.pem /etc/nginx/certs/nginx-"$DOMAIN".key
         fi
     fi
-
-    if [ "$CERT" != "none" ]; then
-        mkdir -p /etc/nginx/certs
-    else
-        for site in `ls -1 /etc/letsencrypt/live | grep -v ^README$`; do
-            cat /etc/letsencrypt/live/$site/privkey.pem \
-              /etc/letsencrypt/live/$site/fullchain.pem \
-              | tee /etc/nginx/certs/nginx-"$site".pem >/dev/null
-        done
-    fi
 fi
-cp /etc/letsencrypt/live/* /etc/nginx/certs/
-
 exit 0
