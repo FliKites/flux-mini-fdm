c188a64fa07d2ebfab231592807640f6e4546956
motailab <mdmotailab@gmail.com>
2023-03-07 12:34:51 +0600

    updated script.

--- service.js
+++ service.js
@@ -8,24 +8,23 @@ const nodecmd = require("node-cmd");
 const util = require("util");
 const timer = require("timers/promises");
 
-const { getFluxNodes, findMostCommonResponse } = require("./utils/utils");
+const {
+  getFluxNodes,
+  findMostCommonResponse,
+  checkConnection,
+} = require("./utils/utils");
 
 const configFile = "/etc/nginx/nginx.conf";
 const appName = process.env.APP_NAME || "explorer";
 const appPort = process.env.APP_PORT || 39185;
-const stickySession = process.env.STICKY || true;
-const statUser = process.env.STAT_USER || null;
-const statPass = process.env.STAT_PASS || null;
-const checkURL = process.env.CHECK_URL || false;
-const checkStatus = process.env.CHECK_STATUS || false;
 const appDomain = process.env.DOMAIN || "";
 const cmdAsync = util.promisify(nodecmd.run);
 
 async function getApplicationIP(app_name) {
   try {
-    // Array of URLs
-    const fluxNodes = await getFluxNodes();
     // Select 5 random URLs
+    const fluxNodes = await getFluxNodes();
+
     const randomFluxNodes = fluxNodes
       .sort(() => 0.5 - Math.random())
       .slice(0, 5);
@@ -73,40 +72,6 @@ function convertIP(ip) {
   return ip;
 }
 
-async function getHAConfig() {
-  let HAconfig = "";
-  const fileStream = fs.createReadStream(configFile);
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
-  for await (const line of rl) {
-    HAconfig += `${line}\n`;
-    if (line.includes("[SERVERS]")) break;
-  }
-  return HAconfig;
-}
-
-function addStats(user, pass) {
-  let HAconfig = fs.readFileSync(configFile).toString();
-  HAconfig = HAconfig.replace(
-    "[STATS]",
-    `[STATS]
-listen stats
-    bind :8080
-    mode http
-    stats enable
-    stats hide-version
-    stats realm Haproxy\\ Statistics
-    stats uri /
-    stats auth ${user}:${pass}
-`
-  );
-
-  fs.writeFileSync(configFile, HAconfig);
-}
-
 async function updateList() {
   while (true) {
     try {
@@ -116,23 +81,6 @@ async function updateList() {
         console.log(`${configFile} not found. trying again...`);
         await timer.setTimeout(500);
       }
-      // let config = await getHAConfig();
-      // if (stickySession === true)
-      //   config +=
-      //     "    cookie FLUXSERVERID insert indirect nocache maxlife 8h\n\n";
-      // if (checkURL)
-      //   config += `    option httpchk\n\n    http-check connect\n    http-check send meth GET uri ${checkURL}\n`;
-      // if (checkURL && checkStatus)
-      //   config += `    http-check expect status ${checkStatus}\n\n`;
-      // for (let i = 0; i < ipList.length; i += 1) {
-      //   const serverIP = convertIP(ipList[i]);
-      //   const serverID = `ip_${serverIP.replaceAll(".", "_")}`;
-      //   let stikyCoockie = "";
-      //   if (stickySession === true) stikyCoockie = `cookie ${serverID}`;
-      //   config += `    server ${serverID} ${serverIP}:${appPort} check ${stikyCoockie}\n`;
-      // }
-      // console.log(config);
-      // fs.writeFileSync(configFile, config);
       replaceServersAndCertInNginxConf(
         ipList.map(convertIP).map((ip) => `${ip}:${appPort}`),
         appDomain,
@@ -172,5 +120,29 @@ function replaceServersAndCertInNginxConf(servers, serverName, certName) {
   fs.writeFileSync(configFile, finalConf);
 }
 
-if (statUser && statPass) addStats(statUser, statPass);
-updateList();
+async function startUP() {
+  const filePath = process.env.CERT_PATH;
+  const certScript = process.env.CERT_SCRIPT_PATH;
+  try {
+    if (!fs.existsSync(filePath)) {
+      let count = 0;
+      while (!fs.existsSync(filePath) && count < 5) {
+        count++;
+        await cmdAsync(certScript);
+        console.log("retrying certs " + count);
+        if (fs.existsSync(filePath)) {
+          updateList();
+          break;
+        }
+        await timer.setTimeout(
+          1000 * (process.env?.RETRY_DELAY ? +process.env?.RETRY_DELAY : 60)
+        );
+      }
+    } else {
+      updateList();
+    }
+  } catch (error) {
+    console.log("error something went wrong ", error?.message);
+  }
+}
+startUP();
