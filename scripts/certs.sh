a7d62cb969c42c0647947fe20b3618b991c2eed5
FliKites <72367027+FliKites@users.noreply.github.com>
2023-04-26 22:25:26 -0600

    Update certs.sh - Add Dual Domain Support
    You can now specify CERT=dual to deploy both a ICANN domain and a Handshake domain on the same instance.
    
    DOMAIN= is to specify ICANN domain
    
    HNS_DOMAIN= is to specify Handshake domain 
    
    It will output the TLSA record for both domains

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -6,6 +6,27 @@ if [ -n "$DOMAIN" ]; then
               /etc/letsencrypt/live/"$DOMAIN".crt \
               | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
               echo "Self-signed certificate manually loaded and stored in /etc/haproxy/certs/"
+   elif [ "$CERT" = "dual" ] && [ -z "$(ls -A /etc/haproxy/certs)" ]; then
+     mkdir -p /etc/letsencrypt/live && mkdir -p /etc/haproxy/certs &&
+openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
+  -keyout /etc/letsencrypt/live/"$HNS_DOMAIN".key -out /etc/letsencrypt/live/"$HNS_DOMAIN".crt -extensions ext  -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[ext]";
+    echo "keyUsage=critical,digitalSignature,keyEncipherment";
+    echo "extendedKeyUsage=serverAuth";
+    echo "basicConstraints=critical,CA:FALSE";
+    echo "subjectAltName=DNS:$HNS_DOMAIN";
+    ) -subj "/CN=$HNS_DOMAIN" && cat /etc/letsencrypt/live/"$HNS_DOMAIN".key \
+              /etc/letsencrypt/live/"$HNS_DOMAIN".crt \
+              | tee /etc/haproxy/certs/haproxy-"$HNS_DOMAIN".pem >/dev/null
+    echo "Self-signed certificate generated and stored in /etc/haproxy/certs/" &&
+
+ certbot certonly --no-self-upgrade -n --text --standalone \
+            --preferred-challenges http-01 \
+            -d "$DOMAIN" --keep --expand --agree-tos --email "$EMAIL" &&  mkdir -p /etc/haproxy/certs && cat /etc/letsencrypt/live/"$DOMAIN"/privkey.pem \
+              /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
+              | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null && echo "Letsencrypt certificate generated and stored in /etc/haproxy/certs/"
 
     elif [ "$CERT" = "self" ] && [ -z "$(ls -A /etc/haproxy/certs)" ]; then
      mkdir -p /etc/letsencrypt/live && mkdir -p /etc/haproxy/certs &&
@@ -22,7 +43,7 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
               /etc/letsencrypt/live/"$DOMAIN".crt \
               | tee /etc/haproxy/certs/haproxy-"$DOMAIN".pem >/dev/null
     echo "Self-signed certificate generated and stored in /etc/haproxy/certs/"
-    
+
     elif [ "$STAGING" = true ]; then
         if [ -n "$ACME" ]; then
             certbot certonly --no-self-upgrade -n --text --standalone \
@@ -57,8 +78,14 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes \
     fi
              TLSA=$(openssl x509 -noout -pubkey -in /etc/haproxy/certs/haproxy-"$DOMAIN".pem | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | sed '' | awk '{print "3 1 1",$1}')
              echo $TLSA > /etc/letsencrypt/live/"$DOMAIN"_TLSA.txt
-             echo "Generated below is the TLSA DNS record - Use With Handshake Domains or DNSSEC | Record Name: _443._tcp.$DOMAIN"
+             echo "Generated below is the TLSA DNS record associated with your certificate | Add This Record Name To DNS: _443._tcp.$DOMAIN"
              echo $TLSA
+         if [ -n "$HNS_DOMAIN" ]; then
+TLSA=$(openssl x509 -noout -pubkey -in /etc/haproxy/certs/haproxy-"$HNS_DOMAIN".pem | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | sed '' | awk '{print "3 1 1",$1}')
+             echo $TLSA > /etc/letsencrypt/live/"$HNS_DOMAIN"_TLSA.txt
+             echo "Generated below is the TLSA DNS record associated with your Handshake Domain | Add This Record Name To DNS: _443._tcp.$HNS_DOMAIN"
+             echo $TLSA
+fi
 fi
 
 exit 0
